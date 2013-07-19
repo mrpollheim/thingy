ec819a2b2b7b8bba0b3a92ce76a6254d80465859
Justin Pollheim <mrpollheim@gmail.com>
2013-07-19 10:11:49 +0930

    Test Commit

---
+++ app/views/statuses/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @status, :country, :content, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Thingy::Application.config.secret_key_base = 'e0a0584e325ff035cd351c566a3162dd7c47788c2da728bf94b76bf759476a8b7d0723008254b58a0b1b30ba5edf5d6526698980baf8b2607d0b22da1e04ec1d'
